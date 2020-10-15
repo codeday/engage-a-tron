1ba9a2973afe295e1286bd74b37102ad0550112b
Lola <lola@egherman.com>
2020-10-15 17:28:53 -0500

    fix one issue, looking for more

--- bot/helpers/role_filter.py
+++ bot/helpers/role_filter.py
@@ -2,7 +2,7 @@ from discord import Member
 
 
 def check_roles(
-    member: Member, include_roles: list = ["Student"], exclude_roles: list = [],
+    member: Member, include_roles: list = ["Community Member"], exclude_roles: list = [],
 ) -> bool:
     member_roles = [r.name for r in member.roles]
     if any(ele in member_roles for ele in include_roles) and not any(
